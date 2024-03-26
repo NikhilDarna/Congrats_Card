f4377c0dd62f21420182798d71c1902336c7bbe0
NikhilDarna <nikhildarna5@gamail.com>
2024-03-26 10:26:19 +0000

    Congrats_Card

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className='bg'>
+    <h1>Congratulations</h1>
+    <div className='container'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+        className='imgSize'
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu institute of computer</p>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'
+        className='imgSize'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
